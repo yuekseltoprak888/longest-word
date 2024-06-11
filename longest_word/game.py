dadc648a7415f6c57019ffa24cc3340a99fd7793
yuekseltoprak <yueksel.toprak@galexis.com>
2024-06-11 19:51:21 +0000

    Game development with TDD

--- longest_word/game.py
+++ longest_word/game.py
@@ -1,8 +1,31 @@
-import string
 import random
+import string
+import requests
+
 
 class Game:
-    def __init__(self):
+    def __init__(self) -> list:
+        """Attribute a random grid to size 9"""
         self.grid = []
         for _ in range(9):
             self.grid.append(random.choice(string.ascii_uppercase))
+
+    def is_valid(self, word: str) -> bool:
+        """Return True if and only if the word is valid, given the Game's grid"""
+        if not word:
+            return False
+        letters = self.grid.copy()  # Consume letters from the grid
+        for letter in word:
+            if letter in letters:
+                letters.remove(letter)
+            else:
+                return False
+
+        is_english = self.__check_dictionary(word)
+        return is_english
+
+    @staticmethod
+    def __check_dictionary(word):
+        response = requests.get(f"https://dictionary.lewagon.com/{word}")
+        json_response = response.json()
+        return json_response["found"]
