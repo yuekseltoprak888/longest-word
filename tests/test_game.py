2378f5906afa13df3d52cfb70502d9f6d5612923
yuekseltoprak <yueksel.toprak@galexis.com>
2024-06-11 19:46:57 +0000

    Game development with TDD

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,33 +1,53 @@
+import string
 from longest_word.game import Game
 
 
-def test_empty_word_is_invalid(self):
-    # setup
-    new_game = Game()
-    # verify
-    assert new_game.is_valid('') is False
+class TestGame:
+    def test_game_initialization(self):
+        # setup
+        new_game = Game()
 
+        # exercise
+        grid = new_game.grid
 
-def test_is_valid(self):
-    # setup
-    new_game = Game()
-    test_grid = 'KWEUEAKRZ'
-    test_word = 'EUREKA'
-    # exercice
-    new_game.grid = list(test_grid) # Force the grid to a test case
-    # verify
-    assert new_game.is_valid(test_word) is True
-    # teardown
-    assert new_game.grid == list(test_grid) # Make sure the grid remained untouched
+        # verify
+        assert type(grid) == list
+        assert len(grid) == 9
+        for letter in grid:
+            assert letter in string.ascii_uppercase
 
-def test_is_invalid(self):
-    # setup
-    new_game = Game()
-    test_grid = 'KWEUEAKRZ'
-    test_word = 'SANDWICH'
-    # exerice
-    new_game.grid = list(test_grid) # Force the grid to a test case
-    # verify
-    assert new_game.is_valid(test_word) is False
-    # teardown
-    assert new_game.grid == list(test_grid) # Make sure the grid remained untouched
+    def test_empty_word_is_invalid(self):
+        # setup
+        new_game = Game()
+        # verify
+        assert new_game.is_valid("") is False
+
+    def test_is_valid(self):
+        # setup
+        new_game = Game()
+        test_grid = "KWEUEAKRZ"
+        test_word = "EUREKA"
+        # exercice
+        new_game.grid = list(test_grid)  # Force the grid to a test case
+        # verify
+        assert new_game.is_valid(test_word) is True
+        # teardown
+        assert new_game.grid == list(test_grid)  # Make sure the grid remained untouched
+
+    def test_is_invalid(self):
+        # setup
+        new_game = Game()
+        test_grid = "KWEUEAKRZ"
+        test_word = "SANDWICH"
+        # exerice
+        new_game.grid = list(test_grid)  # Force the grid to a test case
+        # verify
+        assert new_game.is_valid(test_word) is False
+        # teardown
+        assert new_game.grid == list(test_grid)  # Make sure the grid remained untouched
+
+    def test_unknown_word_is_invalid(self):
+        """A word that is not in the English dictionary should not be valid"""
+        new_game = Game()
+        new_game.grid = list("KWIENFUQW")  # Force the grid to a test case:
+        assert new_game.is_valid("FEUN") is False
